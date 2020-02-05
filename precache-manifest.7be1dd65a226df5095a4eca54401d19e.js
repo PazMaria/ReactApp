0764647ac51422b79465916ff855a0ce979ac753
Maria Paz <pazguada@gmail.com>
2020-02-05 14:39:48 -0700

    Updates

---
+++ precache-manifest.7be1dd65a226df5095a4eca54401d19e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af06df16bf0e19edbec9f91ad8ae6e1c",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "bc7f291995fb4b04a49f",
+    "url": "/ReactApp/static/css/main.10107d82.chunk.css"
+  },
+  {
+    "revision": "6a894887378b05ede8b2",
+    "url": "/ReactApp/static/js/2.cbe2bdc3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactApp/static/js/2.cbe2bdc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7f291995fb4b04a49f",
+    "url": "/ReactApp/static/js/main.355d4abb.chunk.js"
+  },
+  {
+    "revision": "67d30ea8cd996cd25a37",
+    "url": "/ReactApp/static/js/runtime-main.905221a7.js"
+  },
+  {
+    "revision": "823c8a5af6b062ab3e0c036ce9cfcdd1",
+    "url": "/ReactApp/static/media/bank.823c8a5a.svg"
+  },
+  {
+    "revision": "1a5f2c52ca20dca93c9ea7fa58329746",
+    "url": "/ReactApp/static/media/city.1a5f2c52.svg"
+  },
+  {
+    "revision": "41f8de84b50db020ba4f0c78db6d05cf",
+    "url": "/ReactApp/static/media/coins.41f8de84.svg"
+  },
+  {
+    "revision": "e5b2719487181fe4d0d719f680c46ed7",
+    "url": "/ReactApp/static/media/home.e5b27194.svg"
+  },
+  {
+    "revision": "f8b3222159b1c76c9034a68d676ea6ad",
+    "url": "/ReactApp/static/media/link.f8b32221.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ReactApp/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "9c4830bb9b10f342d4e60c1c2474613d",
+    "url": "/ReactApp/static/media/settings.9c4830bb.svg"
+  },
+  {
+    "revision": "463a1fca626716e89ec490f796d6acda",
+    "url": "/ReactApp/static/media/tictac.463a1fca.svg"
+  },
+  {
+    "revision": "9eb4b5e9008466bd41a3a72302108436",
+    "url": "/ReactApp/static/media/world.9eb4b5e9.jpg"
+  }
+]);
